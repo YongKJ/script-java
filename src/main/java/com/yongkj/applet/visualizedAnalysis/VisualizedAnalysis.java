349d47462800efdee62a5e9d8674abb304c7a9c6
dengxiaojun <dxj1718874198@gmail.com>
2023-07-01 08:21:58 +0800

    [feat]Visualized analysis

---
+++ src/main/java/com/yongkj/applet/visualizedAnalysis/VisualizedAnalysis.java
@@ -0,0 +1,49 @@
+package com.yongkj.applet.visualizedAnalysis;
+
+import com.yongkj.util.FileUtil;
+import com.yongkj.util.GenUtil;
+import tech.tablesaw.api.DoubleColumn;
+import tech.tablesaw.api.StringColumn;
+import tech.tablesaw.api.Table;
+import tech.tablesaw.plotly.Plot;
+import tech.tablesaw.plotly.api.LinePlot;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.List;
+
+public class VisualizedAnalysis {
+
+    private final String excelPath;
+    private final String outputPath;
+
+    private VisualizedAnalysis() {
+        excelPath = GenUtil.getValue("excel-path");
+        outputPath = GenUtil.getValue("output-path");
+    }
+
+    private void apply() {
+        Table table = Table.read().file(excelPath);
+        List<String> cols = Arrays.asList(
+                "train_loss", "test_loss",
+                "train_acc", "test_acc"
+        );
+        FileUtil.mkdir(outputPath);
+        for (String col : cols) {
+            Table outputTable = table.selectColumns("epoch");
+            StringColumn stringColumn = table.stringColumn(col);
+            DoubleColumn doubleColumn = stringColumn.parseDouble();
+
+            doubleColumn.setName(col);
+            outputTable.addColumns(doubleColumn);
+            Plot.show(LinePlot.create(
+                    "epoch / " + col, outputTable, "epoch", col
+            ), new File(outputPath + "epoch-" + col + ".html"));
+        }
+    }
+
+    public static void run(String[] args) {
+        new VisualizedAnalysis().apply();
+    }
+
+}
