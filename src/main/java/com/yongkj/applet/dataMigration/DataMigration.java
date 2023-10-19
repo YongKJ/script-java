55eb7109e4b91b5ab786096773de78a085264e4d
dxj <1718874198@qq.com>
2023-10-19 18:52:19 +0800

    [update]data migration

--- src/main/java/com/yongkj/applet/dataMigration/DataMigration.java
+++ src/main/java/com/yongkj/applet/dataMigration/DataMigration.java
@@ -4,10 +4,12 @@ import com.yongkj.applet.dataMigration.pojo.dto.Database;
 import com.yongkj.applet.dataMigration.pojo.dto.Manager;
 import com.yongkj.applet.dataMigration.util.SQLUtil;
 import com.yongkj.pojo.dto.Log;
+import com.yongkj.util.GenUtil;
 import com.yongkj.util.LogUtil;
 
 import java.sql.DatabaseMetaData;
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,36 +19,93 @@ public class DataMigration {
     private Manager desManager;
     private Database srcDatabase;
     private Database desDatabase;
+    private String databaseName;
+    private List<String> tableNames;
 
     private DataMigration() {
         this.srcDatabase = Database.get("src");
         this.desDatabase = Database.get("des");
+        this.tableNames = GenUtil.getList("table-names");
         this.srcManager = SQLUtil.getConnection(srcDatabase);
         this.desManager = SQLUtil.getConnection(desDatabase);
+        this.databaseName = GenUtil.getValue("databse-name");
     }
 
     private void apply() {
-        List<String> catalogNames = getCatalogs(srcManager);
-        LogUtil.loggerLine(Log.of("DataMigration", "apply", "catalogNames", catalogNames));
+        List<String> databases = getDatabases(srcManager);
+        LogUtil.loggerLine(Log.of("DataMigration", "apply", "databases", databases));
+        List<String> tables = getTables(srcManager, databaseName);
+        LogUtil.loggerLine(Log.of("DataMigration", "apply", "tableNames", tables));
+        List<String> fieldNames = getFields(srcManager, databaseName, tableNames.get(0));
+        LogUtil.loggerLine(Log.of("DataMigration", "apply", "fieldNames", fieldNames));
         SQLUtil.closeAll(srcManager);
         SQLUtil.closeAll(desManager);
     }
 
-    private List<String> getCatalogs(Manager manager) {
-        List<String> catalogNames = new ArrayList<>();
+    private List<String> getFields(Manager manager, String database, String table) {
+        List<String> fields = new ArrayList<>();
         try {
+            int columnIndex = 1;
             DatabaseMetaData metaData = manager.getConnection().getMetaData();
-            ResultSet resultSet = metaData.getCatalogs();
-            LogUtil.loggerLine(Log.of("DataMigration", "getCatalogs", "resultSet", resultSet));
+            ResultSet resultSet = metaData.getColumns(null, database, table, "%");
+            LogUtil.loggerLine(Log.of("DataMigration", "getTableNames", "resultSet", resultSet));
             while (resultSet.next()) {
-                String tableCat = resultSet.getString("TABLE_CAT");
-                catalogNames.add(tableCat);
+                String field = resultSet.getString("COLUMN_NAME");
+                String type = resultSet.getString("TYPE_NAME");
+                String remark = resultSet.getString("REMARKS");
+                LogUtil.loggerLine(Log.of("DataMigration", "apply", "field", field));
+                LogUtil.loggerLine(Log.of("DataMigration", "apply", "type", type));
+                LogUtil.loggerLine(Log.of("DataMigration", "apply", "remark", remark));
+
+                ResultSetMetaData resultSetMetaData = resultSet.getMetaData();
+                int fieldLength = resultSetMetaData.getPrecision(columnIndex);
+                int isNullable = resultSetMetaData.isNullable(columnIndex);
+                LogUtil.loggerLine(Log.of("DataMigration", "apply", "fieldLength", fieldLength));
+                LogUtil.loggerLine(Log.of("DataMigration", "apply", "isNullable", isNullable));
+                System.out.println("--------------------------------------------------------");
+                columnIndex++;
+
+                fields.add(field);
             }
             manager.setResultSet(resultSet);
         } catch (Exception e) {
             e.printStackTrace();
         }
-        return catalogNames;
+        return fields;
+    }
+
+    private List<String> getTables(Manager manager, String database) {
+        List<String> tables = new ArrayList<>();
+        try {
+            DatabaseMetaData metaData = manager.getConnection().getMetaData();
+            ResultSet resultSet = metaData.getTables(null, database, "%", new String[]{"TABLE"});
+            LogUtil.loggerLine(Log.of("DataMigration", "getTableNames", "resultSet", resultSet));
+            while (resultSet.next()) {
+                String table = resultSet.getString("TABLE_NAME");
+                tables.add(table);
+            }
+            manager.setResultSet(resultSet);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return tables;
+    }
+
+    private List<String> getDatabases(Manager manager) {
+        List<String> databases = new ArrayList<>();
+        try {
+            DatabaseMetaData metaData = manager.getConnection().getMetaData();
+            ResultSet resultSet = metaData.getCatalogs();
+            LogUtil.loggerLine(Log.of("DataMigration", "getDatabases", "resultSet", resultSet));
+            while (resultSet.next()) {
+                String database = resultSet.getString("TABLE_CAT");
+                databases.add(database);
+            }
+            manager.setResultSet(resultSet);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return databases;
     }
 
     public static void run(String[] args) {
