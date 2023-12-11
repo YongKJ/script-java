d41bd7234c22086f678002150f0047bf75a9b998
dxj <dxj1718874198@gmail.com>
2023-12-11 23:02:01 +0800

    [update]branch checkout

--- src/main/java/com/yongkj/applet/branchCheckout/BranchCheckoutService.java
+++ src/main/java/com/yongkj/applet/branchCheckout/BranchCheckoutService.java
@@ -9,41 +9,53 @@ import com.yongkj.util.GenUtil;
 import com.yongkj.util.LogUtil;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.ListBranchCommand;
+import org.eclipse.jgit.api.MergeCommand;
+import org.eclipse.jgit.api.PullResult;
 import org.eclipse.jgit.lib.Ref;
-import org.eclipse.jgit.transport.JschConfigSessionFactory;
-import org.eclipse.jgit.transport.OpenSshConfig;
-import org.eclipse.jgit.transport.SshTransport;
-import org.eclipse.jgit.transport.Transport;
+import org.eclipse.jgit.transport.*;
 import org.eclipse.jgit.util.FS;
 
 import java.io.File;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 public class BranchCheckoutService {
 
-    private final String branch;
+    private final String configTag;
     private final String configPath;
     private final String projectPath;
     private final String privateKeyPath;
     private final List<String> pullBranchs;
     private final List<String> projectNames;
-    private final Map<String, Object> mapBranchConfig;
+    private final Map<String, Object> mapTagBranch;
+    private final List<Map<String, Object>> branchs;
 
     private BranchCheckoutService() {
-        branch = GenUtil.getValue("branch");
+        branchs = GenUtil.getListMap("branchs");
+        configTag = GenUtil.getValue("config-tag");
         configPath = GenUtil.getValue("config-path");
         pullBranchs = GenUtil.getList("pull-branchs");
-        projectNames = GenUtil.getList("project-name");
         projectPath = GenUtil.getValue("project-path");
+        projectNames = GenUtil.getList("project-names");
         privateKeyPath = GenUtil.getValue("private-key-path");
-        mapBranchConfig = GenUtil.getMap("branch-config-relation");
+        mapTagBranch = GenUtil.getMap("branch-config-relation");
     }
 
     private void apply() {
         for (String projectName : projectNames) {
-            branchCheckOutAndPull(projectName);
+            Map<String, Object> branchInfo = branchs.stream()
+                    .filter(po -> Objects.equals(po.get("project-name"), projectName))
+                    .findFirst().orElse(null);
+
+            String branch = configTag;
+            if (branchInfo != null) {
+                branch = (String) branchInfo.get("branch");
+            }
+            branchCheckOutAndPull(projectName, branch);
+            configCopy(projectName);
+            bootCopy(projectName);
         }
     }
 
@@ -56,18 +68,90 @@ public class BranchCheckoutService {
             }
             FileUtil.copy(srcConfigPath, desConfigPath);
 
-            LogUtil.loggerLine(Log.of("SwitchApplicationConfig", "apply", "srcConfigPath", srcConfigPath));
-            LogUtil.loggerLine(Log.of("SwitchApplicationConfig", "apply", "desConfigPath", desConfigPath));
+            LogUtil.loggerLine(Log.of("BranchCheckoutService", "configCopy", "srcConfigPath", srcConfigPath));
+            LogUtil.loggerLine(Log.of("BranchCheckoutService", "configCopy", "desConfigPath", desConfigPath));
             System.out.println("---------------------------------------------------------------------------------------------");
         }
     }
 
-    private void bootConfigCopy(String projectName) {
+    private void bootCopy(String projectName) {
+        String srcBootPath = getBootPath(projectName, true);
+        if (FileUtil.exist(srcBootPath)) {
+            String desBootPath = getBootPath(projectName, false);
+            if (FileUtil.exist(desBootPath)) {
+                FileUtil.delete(desBootPath);
+            }
+            FileUtil.copy(srcBootPath, desBootPath);
 
+            LogUtil.loggerLine(Log.of("BranchCheckoutService", "bootCopy", "srcBootPath", srcBootPath));
+            LogUtil.loggerLine(Log.of("BranchCheckoutService", "bootCopy", "desBootPath", desBootPath));
+            System.out.println("---------------------------------------------------------------------------------------------");
+        }
     }
 
-    private void branchCheckOutAndPull(String projectName) {
+    private void branchCheckOutAndPull(String projectName, String branch) {
+        String gitPath = getProjectGitPath(projectName);
+        try {
+            Git git = Git.open(new File(gitPath));
 
+            if (!hasRemoteBranch(git, branch)) {
+                branch = (String) mapTagBranch.get(configTag);
+            }
+
+            String refName = "";
+            String branchName = git.getRepository().getBranch();
+            if (!Objects.equals(branchName, branch)) {
+                Ref ref = git.checkout().setName(branch).call();
+                refName = ref.getName();
+            }
+
+            PullResult pullResult = git.pull().setTransportConfigCallback(this::setSshSessionFactory).call();
+            if (pullResult.isSuccessful() && !pullBranchs.contains(branch)) {
+                branchSyncPull(git);
+            }
+
+            String url = git.getRepository().getConfig().getString("remote", "origin", "url");
+
+            git.close();
+
+            LogUtil.loggerLine(Log.of("BranchCheckoutService", "branchCheckOut", "pullResult.isSuccessful()", pullResult.isSuccessful()));
+            System.out.println("---------------------------------------------------------------------------------------------");
+
+            LogUtil.loggerLine(Log.of("BranchCheckoutService", "branchCheckOut", "branchName", branchName));
+            System.out.println("---------------------------------------------------------------------------------------------");
+
+            LogUtil.loggerLine(Log.of("BranchCheckoutService", "branchCheckOut", "refName", refName));
+            System.out.println("---------------------------------------------------------------------------------------------");
+
+            LogUtil.loggerLine(Log.of("BranchCheckoutService", "branchCheckOut", "url", url));
+            System.out.println("---------------------------------------------------------------------------------------------");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void branchSyncPull(Git git) {
+        try {
+            for (String pullBranch : pullBranchs) {
+                PullResult pullRemoteResult = git.pull()
+                        .setRemoteBranchName(pullBranch)
+                        .setFastForward(MergeCommand.FastForwardMode.NO_FF)
+                        .setTransportConfigCallback(this::setSshSessionFactory).call();
+
+                if (pullRemoteResult.isSuccessful()) {
+                    Iterable<PushResult> pushResults = git.push().setTransportConfigCallback(this::setSshSessionFactory).call();
+                    for (PushResult pushResult : pushResults) {
+                        LogUtil.loggerLine(Log.of("BranchCheckoutService", "branchSyncPull", "pushResult.getMessages()", pushResult.getMessages()));
+                        System.out.println("---------------------------------------------------------------------------------------------");
+                    }
+                }
+
+                LogUtil.loggerLine(Log.of("BranchCheckoutService", "branchSyncPull", "pullRemoteResult.isSuccessful()", pullRemoteResult.isSuccessful()));
+                System.out.println("---------------------------------------------------------------------------------------------");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     private void setSshSessionFactory(Transport transport) {
@@ -108,28 +192,18 @@ public class BranchCheckoutService {
         return false;
     }
 
+    private String getBootPath(String projectName, boolean isSrcPath) {
+        return isSrcPath ? getListPath(configPath, projectName, String.format("bootstrap-%s.yml", configTag)) :
+                getListPath(getProjectResourcesPath(projectName), "bootstrap-dev.yml");
+    }
+
     private String getConfigPath(String projectName, boolean isSrcPath) {
-        return isSrcPath ? getListPath(configPath, projectName, getConfigName()) :
+        return isSrcPath ? getListPath(configPath, projectName, String.format("application-%s.yml", configTag)) :
                 getListPath(getProjectResourcesPath(projectName), "application.yml");
     }
 
-    private String getConfigName() {
-        String configName = "application-%s.yml";
-        return String.format(configName, getConfigTag());
-    }
-
-    private String getConfigTag() {
-        String branchKey = branch;
-        if (branchKey.contains("-")) {
-            branchKey = branchKey.split("-")[0];
-        }
-        if (branchKey.contains("/")) {
-            branchKey = branchKey.split("/")[0];
-        }
-        if (mapBranchConfig.containsKey(branchKey)) {
-            return (String) mapBranchConfig.get(branchKey);
-        }
-        return "dev";
+    private String getProjectGitPath(String projectName) {
+        return getListPath(getProjectPath(projectName), ".git");
     }
 
     private String getProjectResourcesPath(String projectName) {
