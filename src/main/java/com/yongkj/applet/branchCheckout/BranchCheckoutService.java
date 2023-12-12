169b7fc29fa37f9d82efd5d5f1e2a543dceb6607
dxj <1718874198@qq.com>
2023-12-12 11:03:03 +0800

    [update]branch checkout

--- src/main/java/com/yongkj/applet/branchCheckout/BranchCheckoutService.java
+++ src/main/java/com/yongkj/applet/branchCheckout/BranchCheckoutService.java
@@ -7,18 +7,14 @@ import com.yongkj.pojo.dto.Log;
 import com.yongkj.util.FileUtil;
 import com.yongkj.util.GenUtil;
 import com.yongkj.util.LogUtil;
-import org.eclipse.jgit.api.Git;
-import org.eclipse.jgit.api.ListBranchCommand;
-import org.eclipse.jgit.api.MergeCommand;
-import org.eclipse.jgit.api.PullResult;
+import org.eclipse.jgit.api.*;
 import org.eclipse.jgit.lib.Ref;
 import org.eclipse.jgit.transport.*;
 import org.eclipse.jgit.util.FS;
+import org.springframework.util.StringUtils;
 
 import java.io.File;
-import java.util.List;
-import java.util.Map;
-import java.util.Objects;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class BranchCheckoutService {
@@ -26,6 +22,7 @@ public class BranchCheckoutService {
     private final String configTag;
     private final String configPath;
     private final String projectPath;
+    private final boolean branchClean;
     private final String privateKeyPath;
     private final List<String> pullBranchs;
     private final List<String> projectNames;
@@ -39,6 +36,7 @@ public class BranchCheckoutService {
         pullBranchs = GenUtil.getList("pull-branchs");
         projectPath = GenUtil.getValue("project-path");
         projectNames = GenUtil.getList("project-names");
+        branchClean = GenUtil.getBoolean("branch-clean");
         privateKeyPath = GenUtil.getValue("private-key-path");
         mapTagBranch = GenUtil.getMap("branch-config-relation");
     }
@@ -49,10 +47,11 @@ public class BranchCheckoutService {
                     .filter(po -> Objects.equals(po.get("project-name"), projectName))
                     .findFirst().orElse(null);
 
-            String branch = configTag;
-            if (branchInfo != null) {
-                branch = (String) branchInfo.get("branch");
+            String branch = branchInfo == null ? getBranchByConfigTag() : (String) branchInfo.get("branch");
+            if (!StringUtils.hasText(branch)) {
+                continue;
             }
+
             branchCheckOutAndPull(projectName, branch);
             configCopy(projectName);
             bootCopy(projectName);
@@ -94,12 +93,20 @@ public class BranchCheckoutService {
         try {
             Git git = Git.open(new File(gitPath));
 
-            if (!hasRemoteBranch(git, branch)) {
-                branch = (String) mapTagBranch.get(configTag);
-            }
-
             String refName = "";
             String branchName = git.getRepository().getBranch();
+            if (!Objects.equals(branchName, branch) && !hasBranch(git, branch, false)) {
+                refName = branchCheckOutAndPull(git, branch);
+
+                LogUtil.loggerLine(Log.of("BranchCheckoutService", "branchCheckOut", "branchName", branchName));
+                System.out.println("---------------------------------------------------------------------------------------------");
+
+                LogUtil.loggerLine(Log.of("BranchCheckoutService", "branchCheckOut", "refName", refName));
+                System.out.println("---------------------------------------------------------------------------------------------");
+
+                return;
+            }
+
             if (!Objects.equals(branchName, branch)) {
                 Ref ref = git.checkout().setName(branch).call();
                 refName = ref.getName();
@@ -108,6 +115,10 @@ public class BranchCheckoutService {
             PullResult pullResult = git.pull().setTransportConfigCallback(this::setSshSessionFactory).call();
             if (pullResult.isSuccessful() && !pullBranchs.contains(branch)) {
                 branchSyncPull(git);
+            }
+
+            if (branchClean) {
+                branchClean(git, branch);
             }
 
             String url = git.getRepository().getConfig().getString("remote", "origin", "url");
@@ -130,9 +141,143 @@ public class BranchCheckoutService {
         }
     }
 
+    private void branchClean(Git git, String branch) {
+        if (!(branch.contains("feat") || branch.contains("fix"))) {
+            return;
+        }
+
+        try {
+            List<Ref> lstBranch = git.branchList().setListMode(ListBranchCommand.ListMode.ALL).call();
+            List<String> lstBranchName = new ArrayList<>();
+            for (Ref branchRef : lstBranch) {
+                if (!branchRef.getName().endsWith(branch) &&
+                        branchRef.getName().contains("dxj") &&
+                        (branchRef.getName().contains("feat") ||
+                                branchRef.getName().contains("fix"))) {
+                    lstBranchName.add(branchRef.getName());
+                }
+            }
+
+            List<String> branchNames = git.branchDelete().setBranchNames((String[]) lstBranchName.toArray()).call();
+
+            LogUtil.loggerLine(Log.of("BranchCheckoutService", "branchClean", "branchNames", branchNames));
+            System.out.println("---------------------------------------------------------------------------------------------");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private String branchCheckOutAndPull(Git git, String branch) {
+        try {
+            if (!(branch.contains("feat") || branch.contains("fix"))) {
+                Ref ref = git.checkout()
+                        .setName(branch)
+                        .setCreateBranch(true)
+                        .setStartPoint(String.format("origin/%s", branch))
+                        .setUpstreamMode(CreateBranchCommand.SetupUpstreamMode.TRACK).call();
+                return ref.getName();
+            }
+            if (branch.contains("feat")) {
+                return branchCheckOutAndPullByFeat(git, branch);
+            }
+            if (branch.contains("fix")) {
+                return branchCheckOutAndPullByFix(git, branch);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
+
+    private String branchCheckOutAndPullByFeat(Git git, String branch) {
+        try {
+            String tempBranch = pullBranchs.stream()
+                    .filter(po -> po.contains("develop")).findFirst().orElse("develop");
+            branchCheckOutAndPullByTemp(git, tempBranch);
+
+            Ref ref = git.branchCreate().setName(branch).call();
+
+            branchSyncPull(git, Collections.singletonList(tempBranch));
+
+            LogUtil.loggerLine(Log.of("BranchCheckoutService", "branchCheckOutAndPullByFeat", "refName", ref.getName()));
+            System.out.println("---------------------------------------------------------------------------------------------");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
+
+    private String branchCheckOutAndPullByFix(Git git, String branch) {
+        try {
+            String tempBranch = pullBranchs.stream()
+                    .filter(po -> po.contains((String) mapTagBranch.get(configTag)))
+                    .findFirst().orElse((String) mapTagBranch.get(configTag));
+            branchCheckOutAndPullByTemp(git, tempBranch);
+
+            Ref ref = git.branchCreate().setName(branch).call();
+
+            branchSyncPull(git, Collections.singletonList(tempBranch));
+
+            LogUtil.loggerLine(Log.of("BranchCheckoutService", "branchCheckOutAndPullByFix", "refName", ref.getName()));
+            System.out.println("---------------------------------------------------------------------------------------------");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
+
+    private void branchCheckOutAndPullByTemp(Git git, String branch) {
+        try {
+            String branchName = git.getRepository().getBranch();
+            if (!Objects.equals(branchName, branch) && !hasBranch(git, branch, false)) {
+                Ref ref = git.checkout()
+                        .setName(branch)
+                        .setCreateBranch(true)
+                        .setStartPoint(String.format("origin/%s", branch))
+                        .setUpstreamMode(CreateBranchCommand.SetupUpstreamMode.TRACK).call();
+                String refName = ref.getName();
+
+                LogUtil.loggerLine(Log.of("BranchCheckoutService", "branchCheckOutAndPullByTemp", "branchName", branchName));
+                System.out.println("---------------------------------------------------------------------------------------------");
+
+                LogUtil.loggerLine(Log.of("BranchCheckoutService", "branchCheckOutAndPullByTemp", "refName", refName));
+                System.out.println("---------------------------------------------------------------------------------------------");
+
+                return;
+            }
+
+            String refName = "";
+            if (!Objects.equals(branchName, branch)) {
+                Ref ref = git.checkout().setName(branch).call();
+                refName = ref.getName();
+            }
+
+            PullResult pullResult = git.pull().setTransportConfigCallback(this::setSshSessionFactory).call();
+
+            LogUtil.loggerLine(Log.of("BranchCheckoutService", "branchCheckOutAndPullByTemp", "pullResult.isSuccessful()", pullResult.isSuccessful()));
+            System.out.println("---------------------------------------------------------------------------------------------");
+
+            LogUtil.loggerLine(Log.of("BranchCheckoutService", "branchCheckOutAndPullByTemp", "branchName", branchName));
+            System.out.println("---------------------------------------------------------------------------------------------");
+
+            LogUtil.loggerLine(Log.of("BranchCheckoutService", "branchCheckOutAndPullByTemp", "refName", refName));
+            System.out.println("---------------------------------------------------------------------------------------------");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
     private void branchSyncPull(Git git) {
+        branchSyncPull(git, new ArrayList<>());
+    }
+
+    private void branchSyncPull(Git git, List<String> branchFilters) {
         try {
             for (String pullBranch : pullBranchs) {
+                if (branchFilters != null && !branchFilters.isEmpty() && branchFilters.contains(pullBranch)) {
+                    continue;
+                }
+
                 PullResult pullRemoteResult = git.pull()
                         .setRemoteBranchName(pullBranch)
                         .setFastForward(MergeCommand.FastForwardMode.NO_FF)
@@ -170,9 +315,10 @@ public class BranchCheckoutService {
         });
     }
 
-    public boolean hasRemoteBranch(Git git, String branch) {
+    private boolean hasBranch(Git git, String branch, boolean isRemote) {
         try {
-            List<Ref> lstBranch = git.branchList().setListMode(ListBranchCommand.ListMode.REMOTE).call();
+            List<Ref> lstBranch = !isRemote ? git.branchList().call() :
+                    git.branchList().setListMode(ListBranchCommand.ListMode.REMOTE).call();
             List<String> branchNames = lstBranch.stream().map(Ref::getName).collect(Collectors.toList());
 
             LogUtil.loggerLine(Log.of("BranchCheckoutService", "hasRemoteBranch", "branchNames.size()", branchNames.size()));
@@ -190,6 +336,11 @@ public class BranchCheckoutService {
             e.printStackTrace();
         }
         return false;
+    }
+
+    private String getBranchByConfigTag() {
+        String branch = (String) mapTagBranch.get(configTag);
+        return pullBranchs.stream().filter(po -> po.contains(branch)).findFirst().orElse("local");
     }
 
     private String getBootPath(String projectName, boolean isSrcPath) {
